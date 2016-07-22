61e6cbeef783068b8cfc2a8b1b6e3d136266c104
amit4alljava <amit4alljava@gmail.com>
2016-07-22 17:05:52 +0530

    Codes

---
+++ JSCodes/UserCRUD/js/models/usermodel.js
@@ -0,0 +1,14 @@
+function User(username,email,city){
+this.username=username;
+this.email = email;
+this.city=city;
+}
+
+var UserOperations={
+userArray:[],
+    
+    add:function(){
+},
+delete:function(){
+}    
+}
